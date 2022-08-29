597e2ecb1323155aebb7290362b0fc27ceb01d4f
hadeel-hub <hadeela_k2050@yahoo.com>
2022-08-29 17:01:16 +0300

    dojo

---
+++ js/dojo/code.js
@@ -0,0 +1,11 @@
+function hide(element) {
+    element.remove();
+}
+
+function custom() {
+   
+    console.log("Ninja was liked");
+}
+function logout(element) {
+    element.innerText = "logout";
+}
