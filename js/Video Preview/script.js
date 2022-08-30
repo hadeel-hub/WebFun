b9cacc4278d80dbd440a481931f3cf73d5ecc2b8
hadeel-hub <hadeela_k2050@yahoo.com>
2022-08-30 11:31:24 +0300

    video

---
+++ js/Video Preview/script.js
@@ -0,0 +1,7 @@
+function play(vid){
+    vid.play();
+}
+
+function pause(vid){
+    vid.pause();
+}
---
+++ js/loop/code.js
@@ -0,0 +1,20 @@
+var i;
+i=num;
+
+for (i=1; i<=20; i++) {
+
+if(i%2!=0) {
+console.log(num);}
+}
+
+for(i=100; i<=100; i--){
+    if(i%3==0) console.log("i--");
+    }
+    
+    x=num;
+    array=[]
+    for(i=4; i<=4,i--;){
+    f(x)=f(i)-1.5
+    
+    }
+
