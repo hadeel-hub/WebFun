82841ef3fabb5488f0f876e74c7896ebf35bddfa
hadeel-hub <hadeela_k2050@yahoo.com>
2022-08-31 14:12:03 +0300

    connections

---
+++ js/connections/script.js
@@ -0,0 +1,14 @@
+console.log("page loaded...");
+ 
+function myFunction1 (){
+    document.querySelector(".card-list-item1").remove();
+}
+
+function myFunction2 (){
+    document.querySelector (".card-list-item2").remove();
+}
+
+function change(){
+document.querySelector (".ja").innerHTML="rama";
+
+}
