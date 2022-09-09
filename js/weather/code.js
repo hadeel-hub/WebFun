145f12ed8deb575af0f6ef7317255ee06cfcf6a4
hadeel-hub <hadeela_k2050@yahoo.com>
2022-09-10 00:04:40 +0300

    flex-columns

--- js/weather/code.js
+++ js/weather/code.js
@@ -1,5 +1,5 @@
-function popMessage(){
-    alert("Weather is loading ...");
+function popMessage(name){
+    alert("Weather is loading ..."+name);
 }
 
 function hide(){
@@ -7,3 +7,29 @@ function hide(){
    
  x.remove();
 }
+var tmprtures = document.querySelectorAll('.tempretures');
+function celToFeh(deg){
+return deg*1.8+32;
+}
+function fehToCel(deg){
+    return (deg-32)*5/9;
+}
+function degree(element){
+    // // console.log(element.value);
+    // console.log(tmprtures);
+    if(element.value=='Fehrenheit'){
+        // change to fehr
+        for(var i=0;i<tmprtures.length;i++){
+            tmprtures[i].children[0].innerText=parseInt(celToFeh(tmprtures[i].children[0].innerText));
+            tmprtures[i].children[1].innerText=parseInt(celToFeh(tmprtures[i].children[1].innerText));
+        }
+
+    }
+    else if(element.value=='Celsius'){
+     for(var i=0; i<tmprtures.length;i++){
+        tmprtures[i].children[0].innerText=parseInt(fehToCel(tmprtures[i].children[0].innerText));
+        tmprtures[i].children[1].innerText=parseInt(fehToCel(tmprtures[i].children[0].innerText));
+     }
+
+        // change to celsius
+    }
